41744478d21288575704af31fdf725b8c421a7b6
maksym <oliynik.maksim188@gmail.com>
2020-03-03 17:28:52 +0200

    add result field to script model

--- src/main/java/com/pragmasoft/study/model/ScriptModel.java
+++ src/main/java/com/pragmasoft/study/model/ScriptModel.java
@@ -1,8 +1,21 @@
 package com.pragmasoft.study.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 public class ScriptModel {
 
     private String id;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
 
     public String getId() {
         return id;
